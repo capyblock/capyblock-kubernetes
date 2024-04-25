7c869ac0d2b208c14ad3857ec3126f79c479620b
Doğukan Metan <dogukanmetan@gmail.com>
2024-04-25 21:05:28 +0300

    Add installation workflow script

---
+++ src/installation-workflow.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# run cri_selector and get output in a variable
+install_kubernetes(){
+  prepare_installation
+  
+  local install_cri
+  # shellcheck disable=SC2034
+  install_cri=$(cri_selector)
+  install_cri
+  
+  install_kubeadm
+  
+  initialize_kubadm
+}
