7f9ad5b5afa6452e9b9d6ece181af37b3bfaccea
Doğukan Metan <dogukanmetan@gmail.com>
2024-04-25 20:49:45 +0300

    Refactor spacing in log function definition

--- src/functions/logging.sh
+++ src/functions/logging.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-log() {
+log(){
     printf "%${TERMINAL_WIDTH}s\n" | tr ' ' '-' | tee -a "$LOG_FILE"
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] [$SCRIPT_NAME] [INFO] - $1" | tee -a "$LOG_FILE"
     printf "%${TERMINAL_WIDTH}s\n" | tr ' ' '-' | tee -a "$LOG_FILE"
