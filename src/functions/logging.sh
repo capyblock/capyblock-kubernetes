d06e6eef72423a469db4450773b2c305a10b3980
Doğukan Metan <dogukanmetan@gmail.com>
2024-05-04 01:54:19 +0300

    Update logging function to print without writing to a log file

--- src/functions/logging.sh
+++ src/functions/logging.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 log(){
-    printf "%${TERMINAL_WIDTH}s\n" | tr ' ' '-' | tee -a "$LOG_FILE"
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [$SCRIPT_NAME] [INFO] - $1" | tee -a "$LOG_FILE"
-    printf "%${TERMINAL_WIDTH}s\n" | tr ' ' '-' | tee -a "$LOG_FILE"
+    printf "%${TERMINAL_WIDTH}s\n" | tr ' ' '-'
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [$SCRIPT_NAME] [INFO] - $1"
+    printf "%${TERMINAL_WIDTH}s\n" | tr ' ' '-'
 }
