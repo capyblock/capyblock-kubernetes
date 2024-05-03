f568947e59c988042d2fcf49d6fdf579a161c66c
Doğukan Metan <dogukanmetan@gmail.com>
2024-05-04 00:43:07 +0300

    Replace custom script building logic with bash-merger in build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,54 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-BUILD_DIR="../build"
-mkdir -p "$BUILD_DIR"
-BUILD_FULL_FILE="$BUILD_DIR/kubernetes_installer.sh"
+# make bash-merger executable
+chmod +x ../bash-merger/src/merge.sh
 
-# Add #!/usr/bin/env bash to the build file
-echo "#!/usr/bin/env bash" > "$BUILD_FULL_FILE"
+# execute merge.sh
+../bash-merger/src/merge.sh -r . -c merge-config.sh
 
-VARIABLES_DIR="variables"
-FUNCTIONS_DIR="functions"
-BUILD_BLOCKS_DIR="build_blocks"
-SELECTORS_DIR="selectors"
-WORKFLOWS_DIR="workflows"
-
-read_script(){
-    # Check if the file is readable
-    if [ -r "$1" ]; then
-        echo "Running $1"
-        grep -v '^#' "$1" >> "$BUILD_FULL_FILE"
-    else
-        echo "Error: Cannot read $1"
-        exit 1
-    fi
-}
-
-# Read all data from the variable files and add to build file
-for script in "$VARIABLES_DIR"/*.sh; do
-    read_script "$script"
-done
-
-# Read all data from the function files and add to build file
-for script in "$FUNCTIONS_DIR"/*.sh; do
-    read_script "$script"
-done
-
-# Read all data from the build block files and add to build file
-for script in "$BUILD_BLOCKS_DIR"/*.sh; do
-    read_script "$script"
-done
-
-# Read all data from the selector files and add to build file
-for script in "$SELECTORS_DIR"/*.sh; do
-    read_script "$script"
-done
-
-# Read all data from the workflow files and add to build file
-for script in "$WORKFLOWS_DIR"/*.sh; do
-    read_script "$script"
-done
-
-# Add the command handler to the build file
-read_script "command_handler.sh"
